1823583b27c203d3c003af366b30ef67b5cae3a5
SilviaCanoS <99064925+SilviaCanoS@users.noreply.github.com>
2024-03-15 23:45:58 -0600

    Efecto azul
    Cuando PacMan se puede comer a los fantasmas

--- PacMan/Assets/_Scripts/FantasmasSpawner.cs
+++ PacMan/Assets/_Scripts/FantasmasSpawner.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FantasmasSpawner : MonoBehaviour
 {
     public GameObject rojo, prefabRojo, rosa, prefabRosa, azul, prefabAzul, naranja, prefabNaranja;
+    public PacMan pacMan;
+    public Material azulMarino;
 
     private void Start()
     {
@@ -17,15 +19,35 @@ public class FantasmasSpawner : MonoBehaviour
     private void Update()
     {
         if (rojo == null)
+        {
             rojo = Instantiate<GameObject>(prefabRojo, transform.position, Quaternion.identity);
-        
+            if (pacMan.efectoAzulActivado) EfectoAzul("Rojo");
+        }
+
         if (rosa == null)
+        {
             rosa = Instantiate<GameObject>(prefabRosa, transform.position, Quaternion.identity);
+            if (pacMan.efectoAzulActivado) EfectoAzul("Rosa");
+        }
 
         if (azul == null)
+        {
             azul = Instantiate<GameObject>(prefabAzul, transform.position, Quaternion.identity);
+            if (pacMan.efectoAzulActivado) EfectoAzul("Azul");
+        }
 
         if (naranja == null)
+        {
             naranja = Instantiate<GameObject>(prefabNaranja, transform.position, Quaternion.identity);
+            if (pacMan.efectoAzulActivado) EfectoAzul("Naranja");
+        }
+    }
+
+    public void EfectoAzul(string colores)
+    {
+        var aux = GameObject.FindGameObjectWithTag(colores).transform.GetChild(0);
+        aux.GetComponent<MeshRenderer>().material = azulMarino;
+        aux = GameObject.FindGameObjectWithTag(colores).transform.GetChild(1);
+        aux.GetComponent<MeshRenderer>().material = azulMarino;
     }
 }
