8c5ce1c17a5fda0e14b7e91b2766de0ad64e713a
SilviaCanoS <99064925+SilviaCanoS@users.noreply.github.com>
2024-03-30 02:37:45 -0600

    Musica para los fantasmas
    Cada vez que se encuentran a cierta distancia empieza a sonar

--- PacMan/Assets/_Scripts/Puntos.cs
+++ PacMan/Assets/_Scripts/Puntos.cs
@@ -29,8 +29,8 @@ public class Puntos : MonoBehaviour
 
     public void Tranquilo()
     {
-        if (puntos.Length >= 5) Cambiar(5, blanco, "Punto");
-        else Cambiar(puntos.Length, blanco, "Punto");
+        if (puntos.Length >= 5) Cambiar(5, blanco, "Punto", .3f);
+        else Cambiar(puntos.Length, blanco, "Punto", .3f);
 
         Invoke("Peligroso", Random.Range(1, 11));
     }
@@ -41,18 +41,18 @@ public class Puntos : MonoBehaviour
         for (int i = 0; i < puntos.Length; i++) puntos[i] = gameObject.transform.GetChild(i).transform;
         puntos = puntos.OrderBy(x => random.Next()).ToArray(); //desordena el arreglo
 
-        if (puntos.Length >= 5) Cambiar(5, verde, "PuntoVere");
+        if (puntos.Length >= 5) Cambiar(5, verde, "PuntoVere", 1);
         
         Invoke("Tranquilo", Random.Range(1, 11));
     }
 
-    public void Cambiar(int numero, Material color, string tag)
+    public void Cambiar(int numero, Material color, string tag, float tamano)
     {
         for (int i = 0; i < numero; i++)
         {
             if (puntos[i] != null)
             {
-                puntos[i].transform.localScale = new Vector3(.4f, .4f, .4f);
+                puntos[i].transform.localScale = new Vector3(tamano, tamano, tamano);
                 puntos[i].GetComponent<MeshRenderer>().material = color;
                 puntos[i].transform.GetChild(0).GetComponent<MeshRenderer>().material = color;
                 puntos[i].gameObject.tag = tag;
