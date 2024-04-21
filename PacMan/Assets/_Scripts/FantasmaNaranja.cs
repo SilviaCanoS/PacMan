03c4634413f268ac1540b08ba1f3dffc457b8d2b
SilviaCanoS <99064925+SilviaCanoS@users.noreply.github.com>
2024-04-21 09:09:48 -0600

    Corrección de los modelados

--- PacMan/Assets/_Scripts/FantasmaNaranja.cs
+++ PacMan/Assets/_Scripts/FantasmaNaranja.cs
@@ -20,7 +20,7 @@ public class FantasmaNaranja : Fantasma
 
         if (puntaciones.efectoAzul)
         {
-            GetComponent<NavMeshAgent>().destination = new Vector3(2, -1.24f, 0.9f);
+            GetComponent<NavMeshAgent>().destination = new Vector3(2, .25f, 0.9f);
 
             var aux = gameObject.transform.GetChild(4);
             aux.GetComponent<MeshRenderer>().material = azulMarino;
